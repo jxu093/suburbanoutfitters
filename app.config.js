bf781e345641b184b4b6a0ab4637fdb6db3b1d21
jxu <jxu000@gmail.com>
2026-01-04 19:08:35 -0500

    update

--- app.config.js
+++ app.config.js
@@ -1,10 +1,12 @@
-// Loads .env files into process.env when running Expo config
 require('dotenv').config();
 
-module.exports = ({ config }) => ({
-  ...config,
-  extra: {
-    ...(config.extra || {}),
-    WEATHER_API_KEY: process.env.WEATHER_API_KEY || null,
-  },
-});
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      // We manually map it here to ensure it survives the local build process
+      WEATHER_API_KEY: process.env.EXPO_PUBLIC_WEATHER_API_KEY,
+    },
+  };
+};
